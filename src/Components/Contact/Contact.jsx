5ec52eba9a075f81832c419dfc6af7c43ff8d8c0
Praveen Prasad <praveenprasad2510@gmail.com>
2023-10-07 14:55:50 +0530

    Updated fonts

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,42 +1,92 @@
-import React from 'react'
+import React from "react";
 
-import "./contact.scss"
+import "./contact.scss";
 
 function Contact() {
   return (
     <div className="contact-container">
+      <div className="contact-part1">
+        <h1 className="heading-office">Head Office</h1>
+        <div className="address-details">
 
-        
-<div className="contact-part1">
-            <h1>Head Office</h1>
-            <p>T.C.4/894,Mohammed Manzil,Pipeline Road,<br />Kowdiar,TVPM</p>
-            <p>Mob: +91 9495831451</p>
-            <p>Mob: +91 9443692838</p>
-            <p>gdconstructions.co.in</p>
+        <p>
+          T.C.4/894,Mohammed Manzil,Pipeline Road,
+          <br />
+          Kowdiar,TVPM
+        </p>
+        <p>Mob: +91 9495831451</p>
+        <p>Mob: +91 9443692838</p>
+        <p>gdconstructions.co.in</p>
+        </div>
 
-            <div className="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2011846.464872574!2d73.89085375624998!3d9.984804100000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0873d09452d4e1%3A0xad5a19fabeb5b8d5!2sGD%20Decorators%20(False%20Ceiling%20Contractors)!5e0!3m2!1sen!2sin!4v1696571460019!5m2!1sen!2sin"  className='map'  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div className="map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2011846.464872574!2d73.89085375624998!3d9.984804100000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0873d09452d4e1%3A0xad5a19fabeb5b8d5!2sGD%20Decorators%20(False%20Ceiling%20Contractors)!5e0!3m2!1sen!2sin!4v1696571460019!5m2!1sen!2sin"
+            className="map"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+
+      <div className="vertical"></div>
+      <div className="contact-part2">
+        <h1 className="heading-enq">Enquiries</h1>
+        <p>
+          For any enquiries, questions or commendations, please call:
+          123-456-7890 <br /> or fill out the following form
+        </p>
+        <div className="form-container">
+          <div className="form-inputs">
+
+            <div className="input-section1">
+
+            <div className="fullname-input">
+              <input type="text" className="input-full" />
+              <label htmlFor="input" className="label">
+                Full Name
+              </label>
+              <div className="underline"></div>
             </div>
-        </div>
-  
-        <div className="vertical"></div>
-        <div className="contact-part2">
-            <h1>Enquiries</h1>
-            <p>For any enquiries, questions or commendations, please call: 123-456-7890 <br /> or fill out the following form</p>
-       <div className="form-container">
-        <div className="form-inputs">
 
-        <input type="text" placeholder='Fullname' />
-        <input type="email" placeholder='Email' />
-        <input type="text" placeholder='Subject' />
-        <textarea name="message" id="" cols="30" rows="10" placeholder='Message'></textarea>
-        <button type='submit'>Submit</button>
-        </div>
-       </div>
-        </div>
+            <div className="email-input">
+              <input type="email" className="input-mail" />
+              <label htmlFor="input" className="label">
+                Email
+              </label>
+              <div className="underline"></div>
+            </div>
+            </div>
 
+            <div className="subject-input">
+              <input type="text" className="input-sub" />
+              <label htmlFor="input" className="label">
+                Subject
+              </label>
+              <div className="underline"></div>
+            </div>
+
+            <div className="message-input">
+              <textarea
+                name="message"
+                id=""
+                cols="30"
+                rows="10"
+                className="input-mess"
+              ></textarea>
+              <label htmlFor="textarea" className="label">
+                Message
+              </label>
+              <div className="underline"></div>
+            </div>
+
+            <button type="submit" id="submit-btn">Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,20 +11,20 @@ function Footer() {
             <img src={Logo} alt="" className='footer-logo'/>
         </div>
         <div className="footer-part2">
-          <h2>Head Office</h2>
+          <h2 className='footer2-heading'>Head Office</h2>
           <p>T.C.4/894,Mohammed Manzil,Pipeline Road,<br />Kowdiar,TVPM</p>
           <p>+91 9495831451</p>
           <p>+91 9443692838</p>
         </div>
         <div className="footer-part3">
-          <h2>Socials</h2>
+          <h2 className='footer3-heading'>Socials</h2>
           <p>Facebook</p>
           <p>Instagram</p>
           <p>Linkedin</p>
         </div>
          <div className="footer-part4">
-          <h2>Enquiries</h2>
-          <button>Contact US</button>
+          <h2 className='footer4-heading'>Enquiries</h2>
+          <button className='footer4-contact-us'>Contact US</button>
         </div> 
 
     </div>
